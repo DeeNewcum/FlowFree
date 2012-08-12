5a17417cb071144950628b4632c10b686b7a5c92
Dee Newcum <dee.newcum@gmail.com>
2012-08-12 15:44:32 -0500

    break into multiple modules

---
+++ Display.pm
@@ -0,0 +1,111 @@
+package FlowFree::Display;
+
+    use strict;
+    use warnings;
+
+    use Data::Dumper;
+
+use constant box_chars => [
+    [qw[    ╹ ┗ ┃ ┛         ]],     # up
+    [qw[    ┗ ╺ ┏ ━         ]],     # right
+    [qw[    ┃ ┏ ╻ ┓         ]],     # down
+    [qw[    ┛ ━ ┓ ╸         ]]  ];  # left
+
+# map from bright colors => darker color pair 
+#       (used to produce a checkerboard effect)
+use constant dark_colors => {
+    8 => 0,
+    9 => 1,
+    10 => 2,
+    11 => 3,
+    12 => 4,
+    13 => 5,
+    14 => 6,
+    15 => 7,
+};
+
+#use constant circle_diacritic => " ⃝";
+use constant circle_diacritic => "";
+#use constant circle_diacritic => "̳";
+
+sub new { bless {}, shift }
+
+sub resize {
+    my ($self, $width, $height) = @_;
+    $self->{grid} = [
+            map {
+                    [ map { ' ' } 1..$height ]
+            } 1..$width
+        ];
+    $self->{gridcolor} = [
+            map {
+                    [ map { '' } 1..$height ]
+            } 1..$width
+        ];
+
+    foreach my $y (0..($height-1)) {
+        foreach my $x (0..($width-1)) {
+            $self->{gridcolor}[$x][$y] = 8      if (($x+$y)%2);
+        }
+    }
+}
+
+
+# color should be one of these:  http://www.mudpedia.org/wiki/Xterm_256_colors
+sub draw_path {
+    my ($self, $color_hi, $start_coord, $direction_list) = @_;
+    
+    my $x = $start_coord->[0];
+    my $y = $start_coord->[1];
+
+    my $color_lo = dark_colors->{$color_hi};
+    $color_lo = $color_hi   if (!defined($color_lo));
+
+    ## draw start-segment
+    my $dir = $direction_list->[0];
+    $self->{grid}[$x][$y] = box_chars->[$dir][$dir] . circle_diacritic;
+    $self->{gridcolor}[$x][$y] = ($x+$y)%2 ? $color_hi : $color_lo;
+    #print "($x,$y)  $dir→$dir\n";
+
+    ## draw intermediate segments
+    for (my $ctr=0; $ctr<@$direction_list-1; $ctr++) {
+        $dir = $direction_list->[$ctr];
+        my $next_dir = $direction_list->[$ctr+1];
+        $x += $::dir_delta[$dir][0];
+        $y += $::dir_delta[$dir][1];
+        $self->{grid}[$x][$y] = box_chars->[::flip($dir)][$next_dir];
+        $self->{gridcolor}[$x][$y] = ($x+$y)%2 ? $color_hi : $color_lo;
+        #print "($x,$y)  $dir→$next_dir\n";
+   }
+
+    ## draw finish-segment
+    $dir = $direction_list->[-1];
+    $x += $::dir_delta[$dir][0];
+    $y += $::dir_delta[$dir][1];
+    $dir = ::flip($dir);
+    $self->{grid}[$x][$y] = box_chars->[$dir][$dir] . circle_diacritic;
+    $self->{gridcolor}[$x][$y] = ($x+$y)%2 ? $color_hi : $color_lo;
+}
+
+
+sub to_string {
+    my ($self) = @_;
+
+    my $string = '';
+    my $height = scalar @{$self->{grid}[0]};
+    my $width  = scalar @{$self->{grid}};
+    foreach my $y (0..($height-1)) {
+        foreach my $x (0..($width-1)) {
+            my $cell = $self->{grid}[$x][$y];
+            if (length($self->{gridcolor}[$x][$y] || '')) {
+                $cell = "\e[48;5;$self->{gridcolor}[$x][$y]m$cell\e[0m";
+                #$cell = "\e[38;5;$self->{gridcolor}[$x][$y]m$cell\e[0m";
+            }
+            $string .= $cell;
+        }
+        $string .= "\n";
+    }
+    return $string;
+}
+
+1;
---
+++ PathSet.pm
@@ -0,0 +1,61 @@
+package FlowFree::PathSet;
+
+# Each instance of this class holds a "path set" -- a collection of paths, one
+# path for each pair of dots.
+#
+# Each path is a series of moves (up, down, left, right) to make.  So a path might be "up, up,
+# right, right, up, up".  Each path is associated with a particular starting point, and a particular
+# color.
+#
+# The pathset is the main thing that we iterate over, when searching for an acceptable solution.
+#
+# When we start iterating, each path only has one move.  We add moves onto the end of each path, as
+# iteration progresses.  As the board gets more filled, paths may end up becoming different lengths,
+# because a path stops growing once 1) it reaches its destination, or 2) it has nowhere to turn.
+
+    use strict;
+    use warnings;
+
+    use Storable qw[dclone];
+    use Data::Dumper;
+
+# you can either pass it an existing PathSet to clone it, or, pass it these parameters to create a
+# brand new object:
+#
+#       - a reference to a FlowFree::InitialBoard object
+#               (because we need to know the starting location of each path)
+sub new {
+    my $class = shift;
+
+    if (ref($_[0]) eq 'FlowFree::PathSet') {
+        my %$clone = %{shift};
+        my $self = {
+            paths => dclone( $clone{paths} ),
+            board => $clone{board},
+        };
+        return bless $self, $class;
+    } else {
+        my $initial_board = shift;
+        my $self = {
+            board => $initial_board,
+        };
+        return bless $self, $class;
+    }
+}
+
+
+# "Laterally" is a direction in the game-tree:  the same direction as "width first".
+#
+# This twiddles different directions on the bottom layer of the tree.
+sub increment_laterally {
+}
+
+
+# "Vertically" is a direction in the game-tree:  the same direction as "depth first".
+#
+# This adds another layer to the bottom of the tree.
+sub increment_vertically {
+}
+
+
+1;
