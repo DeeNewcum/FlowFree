41aeffcbfc6d5b0db5f2d26e84dda3bfa3efbe1b
Dee Newcum <dee.newcum@gmail.com>
2012-08-13 09:25:19 -0500

    start of implementing/testing increment_laterally

---
+++ test/increment_laterally.1.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+
+    use strict;
+    use warnings;
+
+    BEGIN { chdir ".." }
+
+    require "./mainspace.pm";
+    require "./Display.pm";
+    require "./InitialBoard.pm";
+
+    use Const::Fast;
+
+    use Data::Dumper;
+    #use Devel::Comments;           # uncomment this during development to enable the ### debugging statements
+
+
+
+# level 1, "Bonus Pack"
+my $board = new FlowFree::InitialBoard(
+    height => 3,
+    width  => 3,
+    pairs => [
+        [9,     0, 0,       0, 1],
+        [11,    0, 2,       2, 0],
+    ],
+);
+my $display = new FlowFree::Display;                                                                 
+
+$board->draw($display);
+print $display->to_string();
+
+#print Dumper $display;
+
+
